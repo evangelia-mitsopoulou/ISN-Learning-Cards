e2fb65016d5726c69f9c245f877cf48852045b37
Evangelia Mitsopoulou <emitsopo@gess-sipo-als-dock-1-087.ethz.ch>
2013-08-27 10:31:59 +0200

    a) deactivate testing servers b) deactivate log messages c) keep as default server the PFP LMS in getActiveServer (fixed #246, #245) d) commented the global MOBLERDEBUG variable, because it creates confusion in iOS branch.

--- www/scripts/common.js
+++ www/scripts/common.js
@@ -17,7 +17,7 @@ var DEFAULT_SERVER = "PFPLMS";
  *@default hornet
  **/
  
-var MOBLERDEBUG = 1;
+//var MOBLERDEBUG = 1;
 
 /**
  *	A global property/variable that is used to store info about the different servers to which the application can be connected.
@@ -129,7 +129,7 @@ var URLS_TO_LMS = [
  * @param {String}messagestring, the text message to be displayed in the console
  * */
 function moblerlog(messagestring) {
-	var MOBLERDEBUG = 1;
+	var MOBLERDEBUG = 0;
 	
    if (MOBLERDEBUG === 1) {
         console.log(messagestring);
@@ -158,7 +158,7 @@ function getActiveServer(){
  * @function debugActivate
  * * */
 function debugActivate() {
-	var MOBLERDEBUG = 1;
+	var MOBLERDEBUG = 0;
 	moblerlog("debug Activate and the value of MOBLERDEBUG IS"+MOBLERDEBUG);
 	
 	if (MOBLERDEBUG === 0){
